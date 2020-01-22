c78377e80b6a580466ac52a1b6d1b58b3d40023d
CCotrufo <claudio.cotrufo.bck@outlook.com>
2020-01-22 12:29:57 +0100

    publish

---
+++ app/src/main/java/com/fbnotification/NotificationSettings.java
@@ -0,0 +1,6 @@
+package com.fbnotification;
+
+public class NotificationSettings {
+    public static String HubName = "HUB_NAME";
+    public static String HubListenConnectionString = "HUB_ENDPOINT";
+}
